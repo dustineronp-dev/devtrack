edf3354dbc16bdffbc1bee33cf530a7c94eb783b
Dustin Eron Poyaoan <dustineronp@gmail.com>
2026-07-13 21:29:42 +0800

    Add signup route with bcrytp password hasing

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
 const pool = require('./db');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from DevTrack backend! v2');
-});
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
 });
 
 app.get('/db-test', async (req, res) => {
@@ -18,4 +18,24 @@ app.get('/db-test', async (req, res) => {
     console.error(err);
     res.status(500).json({ error: 'Database connection failed' });
   }
+});
+
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await pool.query(
+      'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email',
+      [email, hashedPassword]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Signup failed' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
